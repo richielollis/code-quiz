0e8637795881af0923184120b80e27e1b3353de1
richielollis <richielollisdesktop@richies-mini.lan>
2022-08-06 18:27:43 -0400

    add basic html for quiz and high scores page and add questions array in JS

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+var score = 0;
+
+var questionsArr = [
+
+    { question: 'Commonly used data types DO NOT include:',
+    answers: ['1. strings', '2. booleans', '3. alerts', '4. numbers'],
+    userAnswer: userAnswer,
+    correctAnswer: '3. alerts',
+    score: score }, 
+
+    { question: 'The condition in an if / else statement is enclosed with __________.',
+    answers: ['1. quotes', '2. curly brackets', '3. parenthesis', '4. square brackets'],
+    userAnswer: userAnswer,
+    correctAnswer: '3. parenthesis',
+    score: score},
+
+    {question: 'Arrays in JavaScript can be used to store __________.',
+    answers: ['1. numbers and strings', '2. other arrays', '3. booleans', '4. all of the above'],
+    userAnswer: userAnswer,
+    correctAnswer: '4. all of the above',
+    score: score},
+
+    {question: 'String values must be enclosed within __________ when being assigned to variables.',
+    answers: ['1. commas', '2. curly brackets', '3. quotes', '4. parenthesis'],
+    userAnswer: userAnswer,
+    correctAnswer: '3. quotes',
+    score: score},
+
+    {question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+    answers: ['1. JavaScript', '2. terminal/bash', '3. for loops', '4. console.log'],
+    userAnswer: userAnswer,
+    correctAnswer: '4. console.log',
+    score: score}
+
+];
